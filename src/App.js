b66de2b4f8663caf5fc3e64a89a00da9f1b553fe
David Qorashi <davoud.qorashi@gmail.com>
2016-11-25 15:23:48 -0800

    Setup the project

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+
+class App extends Component {
+	componentWillMount() {
+		const config = {
+			apiKey: 'AIzaSyCEz-QFlLSorDa8_gIvo3GuMYEvGVCWLDs',
+			authDomain: 'manager-app-rn.firebaseapp.com',
+			databaseURL: 'https://manager-app-rn.firebaseio.com',
+			storageBucket: 'manager-app-rn.appspot.com',
+			messagingSenderId: '410325454890'
+		};
+		firebase.initializeApp(config);
+	}
+
+	render() {
+		return (
+			<Provider store={createStore(reducers)}>
+				<View>
+					<Text>Hello!</Text>
+				</View>
+			</Provider>
+		);
+	}
+}
+
+export default App;
