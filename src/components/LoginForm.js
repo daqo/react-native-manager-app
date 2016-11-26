79c2274c0ccea89160518722d3061315b4e96dee
David Qorashi <davoud.qorashi@gmail.com>
2016-11-25 16:57:17 -0800

    Integrate redux-thunk for logging in

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { emailChanged, passwordChanged } from '../actions';
+import { emailChanged, passwordChanged, loginUser } from '../actions';
 import { Card, CardItem, Input, Button } from './common';
 
 class LoginForm extends Component {
@@ -10,6 +10,11 @@ class LoginForm extends Component {
 
 	onPasswordChange(password) {
 		this.props.passwordChanged(password);
+	}
+
+	onButtonPress() {
+		const { email, password } = this.props;
+		this.props.loginUser({ email, password });
 	}
 
 	render() {
@@ -35,7 +40,7 @@ class LoginForm extends Component {
 				</CardItem>
 
 				<CardItem>
-					<Button>
+					<Button onPress={this.onButtonPress.bind(this)}>
 						Login
 					</Button>
 				</CardItem>
@@ -51,4 +56,4 @@ const mapStateToProps = state => {
 	};
 };
 
-export default connect(mapStateToProps, { emailChanged, passwordChanged })(LoginForm);
+export default connect(mapStateToProps, { emailChanged, passwordChanged, loginUser })(LoginForm);
